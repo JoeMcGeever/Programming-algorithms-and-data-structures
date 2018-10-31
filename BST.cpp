1f96f3b3bc6cfbab05183142b67ff0eac7440983
Joseph McGeever <mcgeevej@uni.coventry.ac.uk>
2018-10-31 17:39:10 +0000

    Started task 1 created node structure

---
+++ BST.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <ifstream>
+using namespace std;
+
+//alphabetically store using ascii values (capital letters should not affect)
+
+struct Node
+{
+    int value, frequency = 0;
+    Node* leftChild;
+    Node* rightChild;
+}
+
+vector<int> constructTree()
+{
+    ifstream textFile.txt;
+    vector<string> binaryTree;
+    textFile.open("textFile.txt");
+    while(file>>word)
+    {
+        
+    }
+    textFile.close();
+    
+}
+
+int main()
+{
+    
+}
