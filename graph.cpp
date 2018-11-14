8695d06c7bf6a5cc37969919749b147df1c2fdc8
Joseph McGeever <mcgeevej@uni.coventry.ac.uk>
2018-11-14 11:30:48 +0000

    graph structure. mode structure and edge adding has been implemented

--- graph.cpp
+++ graph.cpp
@@ -3,22 +3,60 @@
 #include <vector>
 using namespace std;
 
+//ADD A NODE MEANS ADDING IT TO THE LIST OF NODES IN THE GRAPH CLASS, ADDING AN EDGE MEANS UPDATING THE ADJACENCY LIST OF BOTH NODES
+
 struct Node
 {
 public:
     int number;
-    vector<Node*> edge;
+    vector<Node> adjacenyList;
 	Node(int number)
     {
 		this->number = number;
 	}
-    void addEdge(Node* node)
-    {
-        this->edge.push_back(node);
-    }
 };
 
-
+class graph
+{
+    public:
+    vector<Node> graph;
+    int posOfOne, posOfTwo;
+    bool oneSet = false, twoSet = false; //used to see if the nodes are present
+    void addNode(int value)
+    {
+        graph.push_back(value);
+        return;
+    }
+    void addEdge(int valueOne, int valueTwo)
+    {
+        for(auto counter = 0; counter<graph.size(); counter++)
+        {
+            if(graph[counter].number==valueOne) //find the value in the vector of nodes in this object of class
+            {
+                posOfOne = counter; //store the node position within the graph
+                oneSet = true;
+            }
+            if(graph[counter].number==valueTwo) //find the value
+            {
+                posOfTwo = counter; //and for the second
+                twoSet = true;
+            }
+        }
+        if(oneSet==true && twoSet==true) //if they exist on the graph
+        {
+            graph[posOfOne].adjacenyList.push_back(graph[posOfTwo]); //ADD THE SECOND VALUE TO THE ADJACENCY LIST OF THE FIRST
+            graph[posOfTwo].adjacenyList.push_back(graph[posOfOne]); //SAME AS ABOVE BUT 1 IS GOIN IN 2
+            
+            
+            return;
+        }
+        else
+        {
+            cout << "One or more nodes not present" << endl;
+            return;
+        }
+    }
+};
 
 
 bool isPath(int v,int w)
@@ -28,13 +66,24 @@ bool isPath(int v,int w)
     
 }
 
+
+
 int main()
 {
-    vector<Node*> graph;
-    Node sourceNode(2);
-    Node nodeOne(5);
-    cout << sourceNode.number << endl;
-    cout << nodeOne.number << endl;
+    graph myGraph;
     
+    myGraph.addNode(1); //0
+    myGraph.addNode(2); //1
+    myGraph.addNode(3); //etc
+    myGraph.addNode(4);
+    myGraph.addNode(5);
+    myGraph.addNode(6);
+    myGraph.addNode(7);
+    myGraph.addNode(8);
     
+    myGraph.addEdge(3, 5);
+    myGraph.addEdge(2, 1);
+    
+    cout << myGraph.graph[4].adjacenyList[0].number << endl;
+   
 }
