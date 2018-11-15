93485120d4f55d373a9d9df47be737373618bbcf
Joseph McGeever <mcgeevej@uni.coventry.ac.uk>
2018-11-15 10:57:05 +0000

    Correctly locates if two nodes can be pathed to one another, just need to write results to text file

--- graph2.cpp
+++ graph2.cpp
@@ -51,6 +51,47 @@ class Graph
         graph[pos1].adjacenyList.push_back(node);
     }
     
+    bool isPathNodeFinder(int value1, int value2)
+    {
+        int pos1 = findPosOfNode(value1);
+        int pos2 = findPosOfNode(value2);
+        bool pathResult = isPath(graph[pos1], graph[pos2]);
+        for(auto counter = 0; counter < graph.size(); counter ++) //resets the visited values
+        {
+            graph[counter].visited = false;
+        }
+        
+        return pathResult;
+    }
+    
+    bool isPath(Node v, Node w)
+    {
+        
+        for(auto counter = 0; counter < v.adjacenyList.size(); counter++) //loop through v's adjacenyList
+        {
+            if(w.number == v.adjacenyList[counter]->number) //if the node is foudn within this v's adjacency list
+            {
+                return true;
+            }
+        }
+        
+        for(auto counter = 0; counter < v.adjacenyList.size(); counter++)
+        {
+            int posOfNode = findPosOfNode(v.adjacenyList[counter]->number); //fidn the position of hte node in the graph vector from the adjacenyList
+            if(graph[posOfNode].visited==false) //only does this when the node in question hasnt already been observed
+            {
+                graph[posOfNode].visited=true; // sets visited to be true so its not visited again
+                int posOfNode = findPosOfNode(v.adjacenyList[counter]->number); //find the position of the node in the graph vector
+                if(isPath(graph[posOfNode], w)==true) //returns true only when the deeper level of recursion returns true for values in v's adjacency list
+                {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+
+    }
     
 };
 
@@ -59,6 +100,16 @@ int main()
     Graph myGraph;
     myGraph.addNode(3);
     myGraph.addNode(5);
+    myGraph.addNode(1);
+    myGraph.addNode(10);
+    myGraph.addNode(4);
     myGraph.addEdge(3, 5);
+    myGraph.addEdge(10, 5);
+    myGraph.addEdge(10, 1);
     cout << myGraph.graph[0].adjacenyList[0]->number << endl;
+    
+    cout << myGraph.isPathNodeFinder(3, 5) << endl;
+    cout << myGraph.isPathNodeFinder(10, 3) << endl;
+    cout << myGraph.isPathNodeFinder(3, 1) << endl;
+    cout << myGraph.isPathNodeFinder(4, 5) << endl;
 }
